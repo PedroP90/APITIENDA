08118d328cd761a491226bc9c00653762cbb8f9f
PedroP90 <ppergim954@g.educaand.es>
2023-12-03 22:27:10 +0000

    inicio

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true
+      // type: 'postgres',
+      // host: "192.168.1.24",
+      // port: 5432,
+      // database: "postgres",
+      // username: "postgres",
+      // password: "pedro",
+      // autoLoadEntities: true,
+      // synchronize: true
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
